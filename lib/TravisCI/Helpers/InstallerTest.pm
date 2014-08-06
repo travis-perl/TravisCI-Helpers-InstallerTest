29eee8dbe6bfed3a6788966743b1aa6e55dc45c9
Graham Knop <haarg@haarg.org>
2014-08-06 07:03:29 -0400

    test branch in import

--- lib/TravisCI/Helpers/InstallerTest.pm
+++ lib/TravisCI/Helpers/InstallerTest.pm
@@ -2,6 +2,17 @@ package TravisCI::Helpers::InstallerTest;
 use strict;
 use warnings;
 our $VERSION = '0.001';
-sub branch { 'master' }
+our $BRANCH = 'master';
+
+sub import {
+  my ($class, $want) = @_;
+  if ($want && $want ne $BRANCH) {
+    warn "This is $BRANCH, not $want!\n";
+    exit 1;
+  }
+  exit 0
+    if $0 eq '-';
+}
+
 1;
 __END__
